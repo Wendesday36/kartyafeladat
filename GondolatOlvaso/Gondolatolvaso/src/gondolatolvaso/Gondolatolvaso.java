639811604950969bc8d61a8970feb95ffb4da710
Wendesday36 <115298982+Wendesday36@users.noreply.github.com>
2023-09-25 09:47:28 +0200

    alap

---
+++ GondolatOlvaso/Gondolatolvaso/src/gondolatolvaso/Gondolatolvaso.java
@@ -0,0 +1,142 @@
+package gondolatolvaso;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Gondolatolvaso {
+
+    static String pakli[] = new String[22];
+
+    public static void main(String[] args) {
+        jatekMenet();
+    }
+
+    private static void jatekMenet() {
+        kirak();
+        for (int i = 0; i < 3; i++) {
+            kirak();
+            int oszlop = melyik();
+            kever(oszlop);
+        }
+
+        ezVolt();
+    }
+
+    private static void kirak() {
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VII"};
+        /* List<String> llista = new ArrayList<String>();
+        for (int i = 0; i < szinek.length - 1; i++) {
+            for (int j = 0; j < ertekek.length; j++) {
+                llista.add(szinek[i] + "_" + ertekek[j]);
+            }
+        }
+        llista.add(szinek[3] + "_" + ertekek[0]);
+        llista.add(szinek[3] + "_" + ertekek[1]);
+        llista.add(szinek[3] + "_" + ertekek[2]);
+       
+       
+
+        for (int i = 0; i < llista.size(); i++) {
+            pakli[i] = llista.get(i);
+        }*/
+
+ /* int oszlop = 1;
+        for (int i = 0; i < pakli.length; i++) {
+            if (oszlop % 3 != 0) {
+                System.out.print(pakli[i]+"\t");
+            }
+           /* else if(sor % 3 == 0)
+            {
+                System.out.println("\t");
+            }
+            else{
+                System.out.println("\n");
+            }
+            oszlop++;
+        }*/
+        int a = 1;
+        for (String szin : szinek) {
+            for (int e = 0; a < 22 && e < ertekek.length; e++) {
+                pakli[a++] = szin + "_" + ertekek[e];
+            }
+        }
+
+        for (int i = 1; i < pakli.length; i++) {
+            String lap = pakli[i];
+            System.out.print(lap + "\t");
+            if (i % 3 == 0) {
+                System.out.println("");
+            }
+        }
+    }
+
+    private static int melyik() {
+        Scanner sc = new Scanner(System.in);
+        boolean jo;
+        int oszlopSzama;
+        do {
+            System.out.print("Melyik oszlop? {1-3}: ");
+            oszlopSzama = sc.nextInt();
+            jo = oszlopSzama >= 1 && oszlopSzama <= 3;
+        } while (!jo);
+
+        return oszlopSzama;
+
+    }
+
+    private static void kever(int oszlopSzama) {
+String[] ujPakli = new String[22];
+        switch (oszlopSzama) {
+            case (1):
+                for (int i = 1; i < 7; i++) {
+                    pakli[i] = pakli[20 - (i - 1) * 3];
+                    pakli[i + 7] = pakli[19 - (i - 1) * 3];
+                    pakli[i + 14] = pakli[21 - (i - 1) * 3];
+                }
+                for (int i = 1; i < pakli.length; i++) {
+                    String lap = pakli[i];
+                    System.out.print(lap + "\t");
+                    if (i % 3 == 0) {
+                        System.out.println("");
+                    }
+                }
+                break;
+            case (2):
+                for (int i = 1; i < 7; i++) {
+                    pakli[i] = pakli[19 - (i - 1) * 3];
+                    pakli[i + 7] = pakli[20 - (i - 1) * 3];
+                    pakli[i + 14] = pakli[21 - (i - 1) * 3];
+                }
+                for (int i = 1; i < pakli.length; i++) {
+                    String lap = pakli[i];
+                    System.out.print(lap + "\t");
+                    if (i % 3 == 0) {
+                        System.out.println("");
+                    }
+                }
+                break;
+            case (3):
+                for (int i = 1; i < 7; i++) {
+                    pakli[i] = pakli[19 - (i - 1) * 3];
+                    pakli[i + 7] = pakli[21 - (i - 1) * 3];
+                    pakli[i + 14] = pakli[20 - (i - 1) * 3];
+                }
+
+                for (int i = 1; i < pakli.length; i++) {
+                    String lap = pakli[i];
+                    System.out.print(lap + "\t");
+                    if (i % 3 == 0) {
+                        System.out.println("");
+                    }
+                }
+                break;
+        }
+    }
+
+    private static void ezVolt() {
+        System.out.println("A gondolt kártya: " + pakli[11]);
+    }
+
+}
